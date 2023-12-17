18d8fdc990baa9c7693624bb7f28f30504db340c
Roman_Batrakov <kyct69@yandex.ru>
2023-12-17 12:17:56 +0300

    Разработал метод сортировки и вывода книжки. Добавил несколько контактов в книгу

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,13 @@ public class Main {
 
         // Добавляем записи в телефонную книгу
         addContact(phoneBook, "Иванов", "123-456-789");
+        addContact(phoneBook, "Петров", "987-654-321");
+        addContact(phoneBook, "Иванов", "111-222-333");
+        addContact(phoneBook, "Сидоров", "555-666-777");
+        addContact(phoneBook, "Петров", "444-555-666");
+
+        // Выводим отсортированный результат
+        printSortedPhoneBook(phoneBook);
 
     }
 
@@ -28,6 +35,17 @@ public class Main {
 
     // Метод для вывода отсортированной телефонной книги по убыванию числа телефонов
     private static void printSortedPhoneBook(Map<String, List<String>> phoneBook) {
+        // Создаем список записей (имя и количество телефонов) для сортировки
+        List<Map.Entry<String, List<String>>> entries = new ArrayList<>(phoneBook.entrySet());
 
+        // Сортируем список по убыванию количества телефонов
+        entries.sort((entry1, entry2) -> Integer.compare(entry2.getValue().size(), entry1.getValue().size()));
+
+        // Выводим отсортированный результат
+        for (Map.Entry<String, List<String>> entry : entries) {
+            String name = entry.getKey();
+            List<String> phoneNumbers = entry.getValue();
+            System.out.println(name + ": " + phoneNumbers.size() + " телефон(ов) - " + phoneNumbers);
+        }
     }
 }
