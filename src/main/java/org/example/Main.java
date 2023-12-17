ba2be7292a1fd17efd816f9cf058cb7e2ab90cee
Roman_Batrakov <kyct69@yandex.ru>
2023-12-17 12:04:56 +0300

    Создаём проект. Создаём главный класс, главный метод, метод добавления контакта в телефоную книгу, метод сортировки телефонной книги

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    // Метод для добавления контакта в телефонную книгу
+    private static void addContact(Map<String, List<String>> phoneBook, String name, String phoneNumber) {
+
+    }
+
+    // Метод для вывода отсортированной телефонной книги по убыванию числа телефонов
+    private static void printSortedPhoneBook(Map<String, List<String>> phoneBook) {
+
+    }
+}
